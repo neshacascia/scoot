3ae85ebd7bd7e8bf89c58a1caa5101f6524b44eb
Nesha Mervin <nesha.mervin@gmail.com>
2023-02-24 00:46:26 -0500

    completed home page design layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,35 @@
 module.exports = {
   content: ['**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        goldenYellow: '#FCB72B',
+        charcoal: '#495567',
+        lightGray: '#939CAA',
+        darkGray: '#333A44',
+      },
+      backgroundImage: {
+        header: "url('/src/assets/home/home-hero-mobile.jpg')",
+        signUp: "url('/src/assets/patterns/semi-circles.svg')",
+      },
+      fontFamily: {
+        heading: ['Space Mono'],
+        body: ['Lexend Deca'],
+      },
+      width: {
+        oval: '56px',
+        image: '311px',
+        app: '34px',
+      },
+      height: {
+        oval: '56px',
+        image: '311px',
+        icon: '500px',
+        signUp: '320px',
+        footer: '438px',
+        app: '34px',
+      },
+    },
   },
   plugins: [],
 };
